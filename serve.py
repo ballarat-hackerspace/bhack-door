963c65fd56a2902859e2f2242758e63dca5ba692
Ballarat Hackerspace <team@ballarathackerspace.org.au>
2015-03-28 10:11:58 +1100

    updated to output json

--- serve.py
+++ serve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+import json
 import time
-
 from os import curdir
 from os.path import join as pjoin
 
@@ -14,42 +14,51 @@ if not testing:
 
 
 class StoreHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         if self.path == '/open':
             if not testing:
-                pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
-                pfr.relays[0].toggle()
-            self.send_message("Hack away")
+                pfr.relays[0].value = 1
+            self.send_message({"message": "Hack away"})
 
         elif self.path == "/close":
             if not testing:
-                pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
-            self.send_message("Door shut")
+                pfr.relays[0].value = 0
+            self.send_message({"message": "Door shut"})
 
         elif self.path.startswith("/enter"):
             time_to_sleep = 3
             if not testing:
-                pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
-                pfr.relays[0].toggle()
+                pfr.relays[0].value = 1
                 time.sleep(time_to_sleep)
-                pfr.relays[0].toggle()
-            self.send_message("Hack away, door will shut behind you in {} seconds".format(time_to_sleep))
+                pfr.relays[0].value = 0
+            self.send_message({"message": "Hack away, door will shut behind you in {} seconds".format(time_to_sleep)})
 
         elif self.path == "/status":
-            status = str(testing)
-            self.send_message(status)
+            if testing:
+                status = -1
+            else:
+                status = pfr.relays[0].value
+            self.send_message({"status": status})
+
+        else:
+            self.send_message({"message":"This is not the end point you're looking for"}, 404)
+
 
     def do_POST(self):
         return do_GET(self)
         
-    def send_message(self, message):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
+    def send_message(self, message, response=200):
+        self.send_response(response)
+        self.send_header('Cache-control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        #self.wfile.write('"{0}"'.format(message).encode())
-        self.wfile.write(message.encode())
-        
+        self.wfile.write(json.dumps(message).encode())
+
+
+if not testing:
+    pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
 
 server = HTTPServer(('', 8080), StoreHandler)
 server.serve_forever()
